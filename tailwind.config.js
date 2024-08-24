fa6b7732c8a2703fab294bde0f05f4db277bc0ef
sabit-pc <sabitmiah29@gmail.com>
2024-08-24 18:47:51 +0600

    navber section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      fontWeight: {
+        extraBold: '900',
+      },
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1400px',
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
