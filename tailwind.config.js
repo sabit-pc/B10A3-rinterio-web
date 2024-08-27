328532fbeab0d635ded551acd7ad69cb9d3392ae
sabit-pc <sabitmiah29@gmail.com>
2024-08-27 18:39:47 +0600

    unique section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
         xl: '1200px',
         xxl: '1400px',
       },
+      animation: {
+        'spin-slow': 'spin 12s linear infinite',
+      }
     },
   },
   plugins: [
